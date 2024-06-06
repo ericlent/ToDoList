4dae672c3ce2d96b4318f7bc5efed325a16373ab
ericlent <ericquaresma@hotmail.com>
2024-06-06 17:37:41 -0300

    Dia 2

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
-import { Text, View, TextInput, TouchableOpacity, FlatList, Alert, Image } from "react-native";
+import { Text, View, TextInput, TouchableOpacity, FlatList, Alert, Image, ScrollView } from "react-native";
 import { styles } from "./styles";
+import { Chore } from "@/src/components/Chore";
 
 export default function Home() {
     const [chores, setChores] = useState<string[]>([]);
     const [choreName, setChoreName] = useState("");
 
     function handleChoreAdd() {
-
         if (chores.includes(choreName.trim())) {
-
+            return Alert.alert("Tarefa Existe", "Já existe uma tarefa na lista com esse nome")
         }
         else if (choreName.trim() == "") {
-
+            return Alert.alert("Tarefa Inválida", "Digite o nome da tarefa")
         }
         else {
             setChores(prevState => [...prevState, choreName.trim()]);
@@ -20,12 +20,26 @@ export default function Home() {
         }
     }
 
+    function handleChoreRemove(name: string) {
+        Alert.alert("Remover", `Remover a tarefa ${name}?`, [
+            {
+                text: "Sim",
+                onPress: () => setChores(prevState => prevState.filter(chore => chore !== name))
+            },
+            {
+                text: "Não",
+                style: "cancel"
+            }
+        ])
+        console.log(`Remover a tarefa ${name}`);
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.title}>
                 <Image
                     source={require("@/assets/images/ToDo.jpg")}
-                    style={{ width: 60, height: 70 }}
+                    style={{ width: 24, height: 30, marginTop: 48 }}
                     resizeMode="stretch" />
 
                 <Text style={styles.titleTo}>
@@ -42,36 +56,65 @@ export default function Home() {
                     placeholder="Adicionar uma nova tarefa"
                     placeholderTextColor={"#6b6b6b"}
                     keyboardType="default"
-                    //onChangeText={text => setParticipantName(text)}
                     onChangeText={setChoreName}
                     value={choreName.trim()}
                 />
 
-                <TouchableOpacity style={styles.button} onPress={handleChoreAdd}>
-                    <Text style={styles.buttonText}>
-                        +
-                    </Text>
-
-                    {/*
+                <TouchableOpacity onPress={handleChoreAdd}>
                     <Image
                         source={require("@/assets/images/hoverfalsetypecreate.png")}
                         style={{ width: 56, height: 56, flex: 1 }}
                         resizeMode="contain"
-
                     />
-                    */}
                 </TouchableOpacity>
             </View>
 
             <View style={styles.form}>
                 <Text style={styles.txtCriadas}>
-                    Criadas
+                    Criadas {chores.length}
                 </Text>
 
                 <Text style={styles.txtConcluidas}>
                     Concluídas
                 </Text>
             </View>
+
+            
+            <FlatList
+                data={chores}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Chore
+                        key={item}
+                        name={item}
+                        onRemove={() => handleChoreRemove(item)} />
+                )}
+                showsVerticalScrollIndicator={false}
+                ListEmptyComponent={() => (
+                    <>
+                        <Text style={styles.listEmptyText}>
+                            Você ainda não tem tarefas cadastradas
+                        </Text>
+                        <Text style={{ color: "gray", fontSize: 13, textAlign: "center" }}>
+                            Crie tarefas e organize seus itens a fazer
+                        </Text>
+                    </>
+                )}
+            />
+            
+            
+            {/*
+            <ScrollView showsVerticalScrollIndicator={false}>
+                {
+                    chores.map(item => (
+                        <Chore
+                            key={item}
+                            name={item}
+                            onRemove={() => handleChoreRemove(item)} />
+                    ))
+                }
+            </ScrollView>
+            */}
 
         </View>
     )
